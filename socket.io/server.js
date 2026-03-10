32e0761c19cf5546cf6de46f337fd66b0b00ca10
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-03-10 23:30:51 +0530

    added socket.io day1

---
+++ socket.io/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  // ...
+  socket.on("message", (msg) => {
+    // console.log("user fired message event");
+    console.log(msg);
+    io.emit(abc);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("server running at 3000");
+});
